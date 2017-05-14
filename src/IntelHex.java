c0ad7d50850d96ac77ddf22e513a58b78fc10490
sezerp <zabczynskipawe@gmail.com>
2017-05-15 00:24:28 +0200

    first working class without prevent against illegal action like to hex file contain complitly different program

--- src/IntelHex.java
+++ src/IntelHex.java
@@ -1,12 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
  *
  * Created by Paweł Zabczyński on 12/05/2017.
  *
+ *
+ * Class to convert and extract data from hex intel object file
+ * Full documentation of this format is on: http://microsym.com/editor/assets/intelhex.pdf
  */
+
+
 public class IntelHex {
     private String oldPath;
     private String newPath;
-    private int[] quantityRecords = new int[2];
+    private LinkedList<String> oldList = new LinkedList<>();
+    private LinkedList<String> newList = new LinkedList<>();
 
 
     /**
@@ -19,17 +30,99 @@ public class IntelHex {
      * -getData        <-------done
      * */
 
+
+    /*constructors*/
+
+    IntelHex(){
+
+    }
+
+    IntelHex(String path){
+        this.oldPath = path;
+        oldList = fileToLinkedList(path);
+    }
+
+    IntelHex(String pathOld, String pathNew){
+        this.oldPath = pathOld;
+        this.newPath = pathNew;
+
+        oldList = fileToLinkedList(pathOld);
+        newList = fileToLinkedList(pathNew);
+    }
+
     /*public methods*/
+    /**
+     * compareHexFile return only changed records in file
+     * */
+    public LinkedList compareHexFile(){
+        LinkedList<String> list = new LinkedList<>();
+        LinkedList<String> retList = new LinkedList<>();
+        LinkedList<Integer> buffList = new LinkedList<>();
 
+        for(String s: this.newList){
+            retList.add(s);
+        }
 
+        int cnt;
 
+        for(String s: this.oldList){
+            cnt =0;
+
+            for(String s1: this.newList){
+                cnt++;
+
+                if(s1.equals(s)){
+                    list.add(s1);
+                }
+            }
+        }
+
+        for(String s: list){
+            cnt=0;
+            for(String s1: this.newList){
+
+                if(s.equals(s1)){
+                    buffList.add(cnt);
+                }
+                cnt++;
+            }
+        }
+        cnt = 0;
+
+        for(Integer x: buffList){
+            retList.remove(x.intValue() - cnt);
+            cnt++;
+        }
+
+        return retList;
+    }
 
 
 
     /*Private methods*/
+
+    private LinkedList fileToLinkedList(String path){
+        LinkedList<String> list = new LinkedList<>();
+        File file = new File(path);
+
+        try{
+            if(file.exists() && file.isFile()){
+                Scanner fileScaner = new Scanner(file);
+                while(fileScaner.hasNext()){
+                    list.add(fileScaner.next());
+                }
+            }
+
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
     private int hexToInt(String s){
         int nInt = 0;
-        int buffInt = 0;
+        int buffInt;
         int cnt = 0;
         char buffCH;
 
@@ -50,15 +143,19 @@ public class IntelHex {
         return nInt;
     }
 
-//    private int nData(String){
-//
-//    }
-
+    /**
+     * getStartCode return first byte(one char in ASCI) of block of data(named record as well) in intel hex object format file. This bite must contain
+     * colon ":". If it dasn't contain colon return null value.
+     * */
     private String getStartCode(String s){
             s = ""+s.charAt(0);
             if(s.equals(":"))return s;else return null;
 
     }
+    /**
+     * getByteCount(Reclen field in the record) return. This field contain number bytes of data in the record.
+     * Reclen field has 2 bytes(4 haracters in hex)
+     * */
 
     private String getByteCount(String s){
         String buffS = "";
@@ -68,6 +165,10 @@ public class IntelHex {
         return buffS;
     }
 
+    /**
+     *getAddres return address in the memory, type of addres. Is used only in records contain data. It's mean olly when
+     *  rectype is 00.
+     * */
     private String getAddress(String s){
         String buffS = "";
 
@@ -78,6 +179,15 @@ public class IntelHex {
         return buffS;
     }
 
+    /**
+     * getRecordType return 1B(2 character) contein  type of record
+     *  - '00' (0h00, dec: 0, bin: 0000 0000)  record contain data and 16 bit address
+     *  - '01' (0h01, dec: 1, bin: 0000 0001) end of file record
+     *  - '02' (0h02, dec: 2, bin: 0000 0010) Extended segment address record
+     *  - '03' (0h03, dec: 3, bin: 0000 0011)
+     *  - '04' (0h04, dec: 4, bin: 0000 0100)
+     *  - '05' (0h05, dec: 5, bin: 0000 0101)
+     * */
     private String getRecordType(String s){
         String buffS = "";
 
@@ -105,37 +215,6 @@ public class IntelHex {
         }
         return buffS;    }
 
-//    private int cntRecords(String namePath){
-//
-//        int cnt = 0;
-//
-//        File oldFile = new File(namePath);
-//        BufferedReader oldReader = null;
-//        String text = null;
-//
-//        try{
-//            oldReader = new BufferedReader(new FileReader(oldFile));
-//            while( (text = oldReader.readLine()) != null){
-//                cnt++;
-//            }
-//
-//        }catch (FileNotFoundException ex){
-//
-//
-//        }catch (IOException ex){
-//
-//        }
-//        finally {
-//            try{
-//                if(oldReader != null){
-//                    oldReader.close();
-//                }
-//            }catch (IOException ex){
-//
-//            }
-//        }
-//        return cnt;
-//    }
 
 
 /*Setters*/
